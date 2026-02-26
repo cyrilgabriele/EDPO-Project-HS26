e38f145fdbe5c411e33f586e014fd325a32a8ab1
Cyril Gabriele <cyril.gabriele@gmail.com>
2026-02-26 17:05:39 +0100

    added the entry file

---
+++ src/main/java/FinanceBro.java
@@ -0,0 +1,12 @@
+package ch.unisg.kafka.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceBro {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceBro.class, args);
+    }
+}
