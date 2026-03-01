839c7e110572ae1796c317a7b4d36d201d5436d6
Cyril Gabriele <cyril.gabriele@gmail.com>
2026-03-01 21:37:52 +0100

    added a consumer of the binance events as well as removed the legacy code of the SuperHero

--- src/main/java/ch/unisg/kafka/spring/controllers/KafkaController.java
+++ src/main/java/ch/unisg/kafka/spring/controllers/KafkaController.java
@@ -1,35 +1,23 @@
 package ch.unisg.kafka.spring.controllers;
 
-import ch.unisg.kafka.spring.model.SuperHero;
 import ch.unisg.kafka.spring.service.ProducerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/kafka")
 public class KafkaController {
 
-    @Autowired
-    private ProducerService<SuperHero> producerService;
 
+    @Autowired
+    private ProducerService producerService;
 
     @GetMapping(value = "/publish")
     public String sendMessageToKafkaTopic(@RequestParam("message") String message) {
         producerService.sendMessage(message);
         return "Successfully published message..!";
-    }
-
-    @PostMapping(value = "/publish")
-    public Map<String, Object> sendObjectToKafkaTopic(@RequestBody SuperHero superHero) {
-        producerService.sendSuperHeroMessage(superHero);
-
-        Map<String, Object> map = new HashMap<>();
-        map.put("message", "Successfully published Super Hero..!");
-        map.put("payload", superHero);
-
-        return map;
     }
 }
